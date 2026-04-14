b5a3f2e79f902284e1ab19776024d4715a23b159
code-novice02 <sjlee1223uc@gmail.com>
2026-04-14 11:10:58 +0900

    change fibonacci to memoization fibonacci

--- Week07_0414_실습/memoization_fibonacci.py
+++ Week07_0414_실습/memoization_fibonacci.py
@@ -1,13 +1,19 @@
 cnt = 0
+memo = {}
 def fibonacci(n):
     global cnt
     cnt += 1
+    if n in memo:
+        return memo[n]
     if n == 0:
-        return 0
+        memo[n] = 0
     elif n == 1:
-        return 1
+        memo[n] = 1
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        print(memo)
+    return memo[n]
+
 
 n = int(input("N >> "))
 print(f"{n}번째 피보나치 수: {fibonacci(n)}")
